4faf8faa9b85a04cf813ffdbdb209f2066d35dad
Thomas Stachl <thomas@stachl.me>
2022-07-09 01:54:04 +0000

    reenable REPO environmental variable fixing #8

--- entrypoint.fish
+++ entrypoint.fish
@@ -4,6 +4,15 @@
 if age --decrypt --output "$HOME/key.txt" "$(chezmoi source-path)/key.txt.age"
   chmod 600 "$HOME/key.txt"
   chezmoi apply
+
+  if test -n "$REPO" 
+    if test -e "$REPO"
+      cd $REPO && git pull
+    else
+      git clone git@github.com:$REPO $REPO && cd $REPO
+    end
+  end
+
   exec fish
 end
 
