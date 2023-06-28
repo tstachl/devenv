30fe5f76436655c9d38bf9b0f2aa6894c7a0ee0c
Thomas Stachl <286093+tstachl@users.noreply.github.com>
2023-06-28 22:31:11 +0200

    Update entrypoint.sh
    Change to fish

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-if yadm pull && yadm bootstrap && yadm decrypt; then
-
-  if [ -d "$REPO" ]; then
-    cd $REPO && git pull
-  else
-    git clone git@github.com:$REPO $REPO && cd $REPO
-  fi
-
-  clear
-  exec bash --login
+if [ -d "$REPO" ]; then
+  cd $REPO && git pull
+else
+  git clone git@github.com:$REPO $REPO && cd $REPO
 fi
 
-echo "Not allowed to use this."
-exit
+clear
+exec fish --login
